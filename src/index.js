f85b520b514e064a00b64586fc890ca26d89b280
Zaahiid <zahidrsheikh01@gmail.com>
2024-11-10 15:30:16 +0530

    Modified the Folder structure

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/db.js";
 
-import userRoutes from "../routes/user.routes.js"
-
+import userRoutes from "./routes/user.routes.js"
 dotenv.config();
 const port = process.env.PORT || 5000
 
