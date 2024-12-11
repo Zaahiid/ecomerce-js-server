ce72bac9642d3df10579c5b3642724e8a1b1ce5e
Zaahiid <zahidrsheikh01@gmail.com>
2024-12-11 23:23:20 +0530

    Added Product controllers 🎛 and routes 🛣

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import connectDB from "./db/db.js";
 import userRoutes from "./routes/user.routes.js";
 import categoryRoutes from "./routes/category.routes.js";
 import orderRoutes from "./routes/order.routes.js";
+import productRoutes from "./routes/product.routes.js";
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -18,5 +19,6 @@ app.use(express.json());
 app.use("/api/users", userRoutes);
 app.use("/api/category", categoryRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/products", productRoutes);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
