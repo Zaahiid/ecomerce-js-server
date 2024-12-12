466f91e0b3dc6067d93d34919b8057948ea1056c
Zaahiid <zahidrsheikh01@gmail.com>
2024-12-12 21:34:40 +0530

    Added Upload routes ⬆

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/db.js";
+import path from "path";
+import cookieParser from "cookie-parser";
 
 import userRoutes from "./routes/user.routes.js";
 import categoryRoutes from "./routes/category.routes.js";
 import orderRoutes from "./routes/order.routes.js";
 import productRoutes from "./routes/product.routes.js";
+import uploadRoutes from "./routes/upload.routes.js";
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -15,10 +18,16 @@ connectDB();
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 app.use("/api/category", categoryRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/upload", uploadRoutes);
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
